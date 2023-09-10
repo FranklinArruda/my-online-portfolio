926dfea4560cf056cf78e1fa89883ba5eb68a290
Franklin Arruda <102427836+Franklin-93@users.noreply.github.com>
2023-09-10 20:48:49 +0100

    Semantic HTML & CSS Done

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,10 @@
 const readMoreButton = document.querySelector('.read-more-btn');
 readMoreButton.addEventListener('click', toggleReadMore);
 
+// read more function
 function toggleReadMore(event){
     event.preventDefault();
+    
     const readMoreContainer = document.querySelector('.read-more-text');
     if (readMoreContainer.style.display === "none" || readMoreContainer.style.display === "") {
         readMoreContainer.style.display = "block";
