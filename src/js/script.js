f30db28c76bcc13e7382ae9c0cb77659c3112375
Franklin Arruda <102427836+Franklin-93@users.noreply.github.com>
2023-09-21 16:08:34 +0100

    Home

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,9 @@
 const readMoreButton = document.querySelector('.read-more-btn');
-readMoreButton.addEventListener('click', toggleReadMore);
+readMoreButton.addEventListener('click', startReadMore);
 
-// read more function
-function toggleReadMore(event){
+// Read more function
+function startReadMore(event){
     event.preventDefault();
-    
     const readMoreContainer = document.querySelector('.read-more-text');
     if (readMoreContainer.style.display === "none" || readMoreContainer.style.display === "") {
         readMoreContainer.style.display = "block";
@@ -13,4 +12,26 @@ function toggleReadMore(event){
         readMoreContainer.style.display = "none";
         readMoreButton.textContent = "Read more...";
     }
-}
+};
+
+
+/**
+ * When hovers over the download CV button it hides the"download" message and only icon remains
+ * When mouse leaves appears again
+ */
+const btnContainer = document.querySelector(".section1");
+const btnText = document.querySelector(".download-btn");
+
+btnContainer.addEventListener("mouseenter", hideText);
+function hideText(event){
+    btnText.style.display = 'none'; // Hide the icon on mouse enter
+};
+
+btnContainer.addEventListener("mouseleave", showText);
+function showText(event){
+    btnText.style.display = 'inline-block'; // Hide the icon on mouse enter
+};
+
+
+
+ 
