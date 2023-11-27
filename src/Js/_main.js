6dd3d2aa3f0c45aae30c03fbab459720cbb39d20
Franklin Arruda <102427836+Franklin-93@users.noreply.github.com>
2023-11-27 13:53:09 +0000

    scrollbar customized and scroll up button added on scroll

---
+++ src/Js/_main.js
@@ -0,0 +1,45 @@
+import { readMore,readMoreEventListener } from "./readMore.js";
+import { hideShowBar } from "./menuBar.js"; 
+import { revealFunction } from "./scrollReveal.js";
+import { showScrollUpBtn } from "./scrollButton.js";
+
+
+// Call the setup function wherever it's needed, for example, after the DOM is loaded
+document.addEventListener('DOMContentLoaded', function () {
+
+    revealFunction();
+    
+    // Read more function
+    const readMoreButton = document.querySelector('.read-more-btn');
+    readMoreButton.addEventListener('click', function(event) {
+        readMore(event, readMoreButton);
+    });
+
+    // Get all elements with the class "read-more"
+    const readMoreCards = document.querySelectorAll('.read-more, .hide-card-text');
+    readMoreCards.forEach(readMoreCard => { // Add click event listener to each "read-more" element
+        readMoreCard.addEventListener('click', event => {
+            readMoreEventListener(event, readMoreCard); // Pass the specific readMoreCard clicked
+        });
+    });
+
+    // Menu icon (hide and show)
+    const menuIcon = document.getElementById("menu-icon");
+    menuIcon.addEventListener('click', function(event) {
+        hideShowBar(event);
+    });
+
+    window.addEventListener("scroll", () => {
+        showScrollUpBtn();
+    });
+
+
+});
+
+
+
+
+
+ 
+
+
